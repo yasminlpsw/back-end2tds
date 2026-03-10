9fa526e92dc54d96a5c68146c8de04e66aa48e21
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-03-10 11:42:41 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,12 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade; 
+console.log(endereco);
+//console.log(endereco);
+console.log("O endereço é: " endereco)
+
+
+
