ae4ff71384480442778be907c4880889807aebee
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-03-11 08:48:59 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,13 @@
+//Relatorio de notas 
+let aluno = "Gabriel";
+let nota1 = 8.5;
+let nota2 = 5.0;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno: " + aluno + " | Notas:" + nota1 + " e " + nota2; "| Média de notas: " + media;
+console.log("Relatorio de Notas")
+console.log(relatorio);
+
+
+
+
