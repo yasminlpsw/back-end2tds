086c37c74946027ce0b1ef713c7bf85e721ea463
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-03-24 14:42:06 +0000

    versao1

---
+++ interpolacao.js/interpolacao.js
@@ -0,0 +1,11 @@
+const nome = "paula";
+const idade = 26;
+const anoAtual = 2026;
+
+const idadeEmDezAnos = idade + 10;
+const anoNascimento = anoAtual - idade;
+
+console.log("O ano de nascimento é: + anoNascimento");
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui a 10 anos, terá ${idadeEmDezAnos}anos.`);
+//não copiar - teclado acento segundo o shift pra ultilizar crase e depois espaço
