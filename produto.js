e466e6333fe2760aa74e0626cecd800118f932c0
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-03-25 11:59:19 +0000

    v1

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = "Mochila Impermeavel";
+let preco = 189.90
+let estoque = 14;
+const categoria = "acessoórios";
+
+console.log(`A ${produto} custa R$${preco.toFixed}.`);
+console.log(`Temos ${estoque} itens no estoque.`);
+console.log(`O produto pertence a categoria ${categoria}.`);
