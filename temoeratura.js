e24b310750987b15a137cf027f1f8d10e21a2223
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-03-25 12:00:44 +0000

    v1

---
+++ temoeratura.js
@@ -0,0 +1,13 @@
+const cidade = "Curitiba"
+const tempcelsius = 19;
+
+ const tempFahrenheit = tempCelcius *(9 / 5) + 32
+const tempKevin = tempCelsius + 273.15;
+
+console.log(`A temperatura em ${cidade} está ${tempcelcius}Cº`);
+console.log(`\nCelsius: ${tempCelsius}Cº`);
+console.log(`Fahreinheth: ${tempFarenheit}Fº`);
+console.log(`Kelvin ${tempKevin}Kº`);
+console.log(`Variação prevista: ${variaçãoDiaria}`);
+
+
