d5f6e7885d2114b9ae351c47271bd9ab20aa86ba
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-05-11 13:55:38 +0000

    versao1

---
+++ teste.js
@@ -0,0 +1,17 @@
+   
+
+const destino = "paris";
+const nomeViajante = "Caroline";
+const cidadeOrigem = "Curitiba";
+let duracao = 8;
+let transporte ="Carro";
+
+console.log(`O viajante ${nomeViajante} deseja viajar de ${cidadeOrigem} até ${destino}, de ${transporte} em um percurso de ${duracao} horas.`);
+
+let custoPassagem = 5.200
+let hospedagem = 700
+let alimentacao = 70 = 160
+
+let total = hospedagem + alimentacao;
+
+console.log("O total dos gastos será r$" + total + "reais.");
