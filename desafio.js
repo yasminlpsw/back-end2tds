9e71149165aa3dbb93d73e83d2369b520da2fef5
YAS <yasmin.pierchack@escola.pr.gov.br>
2026-05-13 13:50:13 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+
+const nome = Yamal ;
+const idade = 18 ;
+const posicao = "Ponta-direita";
+const time = "barcelona";
+const altura = 1.80;
+const pesoEmKilos = 72;
+
+console.log(`O jogadores ${nome}, segue como ${time}, no time espanhol ${idade} ano de idade, já  se encontra em tamanho com ${altura} de altura e ${pesoEmKilos} quilos.`)
